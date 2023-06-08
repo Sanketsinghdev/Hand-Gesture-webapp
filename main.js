cc1965018858716e10cfac05e816250a8f79a964
Sanketsinghdev <123373053+Sanketsinghdev@users.noreply.github.com>
2023-06-08 17:57:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+https://teachablemachine.withgoogle.com/models/oTYVqVa58/
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
